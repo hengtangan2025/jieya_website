0f0cb9ba31c241753367006d43c7876d5c593ee4
wmrs <15805652230@163.com>
2020-09-16 16:50:48 +0800

    产品服务

--- src/js/service.js
+++ src/js/service.js
@@ -2,11 +2,11 @@ import * as Post from './post';
 var policStr = ""
 Post.service_top.forEach(e => {
     policStr += `<div>
-<div class="uk-border-rounded service_background uk-padding-small">
+<div class="uk-border-rounded service_background uk-padding-small uk-padding-remove-bottom">
 <a href="${e.img}" class="uk-inline">
-<img src="${e.img}" alt="" class="uk-padding-small uk-padding-remove-horizontal">
+<img src="${e.img}" alt="" >
 </a>
-<div class="uk-padding-small uk-padding-remove-horizontal uk-padding-remove-top">${e.name}</div>
+<div class="uk-padding-small uk-padding-remove-horizontal font-size24">${e.name}</div>
 </div>
 </div>`
 })
@@ -14,11 +14,11 @@ document.getElementById("service_top_data").innerHTML = policStr;
 var bottomStr = ""
 Post.service_bottom.forEach(e => {
     bottomStr += `<div>
-<div class="uk-border-rounded service_background uk-padding-small">
+<div class="uk-border-rounded service_background uk-padding-small uk-padding-remove-bottom">
 <a href="${e.img}" class="uk-inline">
-<img src="${e.img}" alt="" class="uk-padding-small uk-padding-remove-horizontal">
+<img src="${e.img}" alt="">
 </a>
-<div class="uk-padding-small uk-padding-remove-horizontal uk-padding-remove-top">${e.name}</div>
+<div class="uk-padding-small uk-padding-remove-horizontal font-size24">${e.name}</div>
 </div>
 </div>`
 })
