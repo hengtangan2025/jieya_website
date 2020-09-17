fca6b11e4af5501de77c2e898bbf776dcc913a57
wmrs <15805652230@163.com>
2020-09-17 14:36:38 +0800

    产品服务

--- src/js/home.js
+++ src/js/home.js
@@ -16,9 +16,9 @@ Post.index_section.forEach(e => {
 <img src="${e.img}" alt="" class="banner_bottom_image">
 </div>
 <div class="uk-h3">${e.name}</div>
-<div class="uk-padding uk-padding-remove-horizontal font-size20">${e.dec}</div>
+<div class="uk-padding uk-padding-remove-horizontal font-size20 ">${e.dec}</div>
 <div uk-lightbox>
-<a class="uk-button uk-button-default uk-border-pill" href="${e.href_img}">查看联系方式</a>
+<a class="uk-button uk-button-default uk-border-pill service_background" href="${e.href_img}">查看联系方式</a>
 </div>
 </div>
 </div>`
